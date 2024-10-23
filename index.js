025dab8691aa176bdb5177edc648913916718eb4
CARLOS DANIEL MORAIS PIRES <carlos0794@digitalcollegesul.local>
2024-10-23 13:18:39 -0300

    bia finalizada

--- index.js
+++ index.js
@@ -23,14 +23,14 @@ numero.onchange = () => {
             mensagem.innerHTML = `Parabéns o número é: ${numeroGerado}`;
             resultado.style.display = 'block';
             numero.disabled = true;
-            bia.src = "bia-sorrindo.png";
+            bia.src = "feliz.png";
             reiniciar.innerText = 'Jogar novamente';
             reiniciar.style.display = 'block';
         }
         numero.value = '';
         palpites.innerHTML = tentativas.join('-');
     } else {
-        bia.src = "bia-chorando.png";
+        bia.src = "triste.png";
         mensagem.innerHTML = `Você não acertou o número era: ${numeroGerado}`;
         resultado.style.display = 'block';
         reiniciar.style.display = 'block';
