13c294ad4c822ca0305412c67f06fd189df277ad
CARLOS DANIEL MORAIS PIRES <carlos0794@digitalcollegesul.local>
2024-10-23 13:11:44 -0300

    Bia finalizada

--- index.js
+++ index.js
@@ -1,46 +1,43 @@
-let numeroGerado = parseInt(Math.ramdom() * 100);
+let numeroGerado = parseInt(Math.random() * 100);
+console.log(numeroGerado);
 let tentativas = [];
 
 numero.onchange = () => {
+    let numeroDigitado = Number(numero.value);
     if(tentativas.length < 10){
-        if(tentativas.length > 0) {
-            for(let i = 0; i < tentativas.length; i++) {
+        if(tentativas.length > 0){
+            for(let i = 0; i < tentativas.length; i++){
                 if(tentativas[i] == numeroDigitado){
-                    alert("este numero já foi digitado");
+                    alert("Este número ja foi digitado");
                     numero.value = '';
                     return;
                 }
             }
         }
-    tentativas.push(numeroDigitado);
-    if(numeroDigitado > numeroGerado)
-        alert('Bia está pensando em um numero menor');
-
-    numero.value ='';
-    palpites.innerHTML = tentativas.join('-');
-    console.log(tentativas);
-} else{
-    alert("Acabou as tentativas")
+        tentativas.push(numeroDigitado);
+        if(numeroDigitado > numeroGerado){
+            alert('Bia esta pensando em numero menor')
+        }else if(numeroDigitado < numeroGerado){
+            alert('Bia esta pensando em numero maior')
+        }else{
+            mensagem.innerHTML = `Parabéns o número é: ${numeroGerado}`;
+            resultado.style.display = 'block';
+            numero.disabled = true;
+            bia.src = "bia-sorrindo.png";
+            reiniciar.innerText = 'Jogar novamente';
+            reiniciar.style.display = 'block';
+        }
+        numero.value = '';
+        palpites.innerHTML = tentativas.join('-');
+    } else {
+        bia.src = "bia-chorando.png";
+        mensagem.innerHTML = `Você não acertou o número era: ${numeroGerado}`;
+        resultado.style.display = 'block';
+        reiniciar.style.display = 'block';
+    }
 }
 
 
-
-
-
-
-
-
-
-
-
-
-// // while(tentativas <= 10){
-// //     //verifica se é um número válido
-// //     if(!/[0-9]/.test(Number(numero.value))){
-// //         alert("Escreva um número válido");
-// //         numero.value ='';
-// //     }else;{
-// //         tentativas++;
-// //     }
-    
+reiniciar.onclick = () => {
+    window.location.reload();
 }
